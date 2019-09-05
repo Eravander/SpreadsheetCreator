8f0704fc0c93597bccdb2518aeafac2478996990
Dominick Geronimo <lorddom22@gmail.com>
2019-09-04 21:33:23 -0400

    UI code

---
+++ SpreadSheet Creator/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace SpreadSheet_Creator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.genBtn = new System.Windows.Forms.Button();
+            this.cancelBtn = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // genBtn
+            // 
+            this.genBtn.Location = new System.Drawing.Point(225, 72);
+            this.genBtn.Name = "genBtn";
+            this.genBtn.Size = new System.Drawing.Size(75, 23);
+            this.genBtn.TabIndex = 0;
+            this.genBtn.Text = "Generate";
+            this.genBtn.UseVisualStyleBackColor = true;
+            this.genBtn.Click += new System.EventHandler(this.GenBtn_Click);
+            // 
+            // cancelBtn
+            // 
+            this.cancelBtn.Location = new System.Drawing.Point(225, 143);
+            this.cancelBtn.Name = "cancelBtn";
+            this.cancelBtn.Size = new System.Drawing.Size(75, 23);
+            this.cancelBtn.TabIndex = 1;
+            this.cancelBtn.Text = "Cancel";
+            this.cancelBtn.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(51, 72);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 94);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.ListBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(343, 238);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.cancelBtn);
+            this.Controls.Add(this.genBtn);
+            this.Name = "Form1";
+            this.Text = "Spreadsheet Creator";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button genBtn;
+        private System.Windows.Forms.Button cancelBtn;
+        private System.Windows.Forms.CheckedListBox listBox1;
+    }
+}
+
---
+++ SpreadSheet Creator/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpreadSheet_Creator
+{
+    public partial class Form1 : Form
+    {
+        List<string> list;
+        public Form1()
+        {
+            InitializeComponent();
+            list = new List<string>();
+            listBox1.Items.AddRange(new string[] { "FA19", "SP20" });
+            listBox1.ItemCheck += new ItemCheckEventHandler(ListBox1_ItemCheck);
+        }
+
+        private void GenBtn_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (string str in list)
+                sb.AppendLine(str);
+            MessageBox.Show(sb.ToString());
+        }
+
+        private void ListBox1_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            string item = listBox1.SelectedItem.ToString();
+            if (e.NewValue == CheckState.Checked)
+            {
+                if (!list.Contains(item))
+                    list.Add(item);
+            }
+            else
+            {
+                if (list.Contains(item))
+                    list.Remove(item);
+            }
+        }
+
+        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
